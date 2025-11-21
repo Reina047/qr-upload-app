1a44e8d218f92321a30cf5f2bcece333fb3ee214
Reina047 <mansourmanel7@gmail.com>
2025-11-21 22:06:40 +0100

    Fix MIME type issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,22 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1)
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img'
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js'
+            }
+        }
     }
 })
